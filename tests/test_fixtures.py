ce589ac917211974691f6c93416a1bdd6b2e6e50
Hagen Wierstorf <hwierstorf@audeering.com>
2023-04-05 10:33:48 +0200

    Add package structure and db test fixture (#10)
    * Add package structure and db test fixture
    
    * Remove dev from flake8 definition
    
    * Rename test file
    
    * Rename
    
    * Fix first name
    
    Co-authored-by: BahaEddine Abrougui <38329838+Bahaabrougui@users.noreply.github.com>
    
    * Splitup fixture
    
    * Try to fix db fixture
    
    * Fix db fixture
    
    * Add another fixture test
    
    * Add missing audplot dependency
    
    * Remove argument as fixture has autouse
    
    ---------
    
    Co-authored-by: BahaEddine Abrougui <38329838+Bahaabrougui@users.noreply.github.com>

---
+++ tests/test_fixtures.py
@@ -0,0 +1,27 @@
+import pytest
+
+import audb
+
+
+def test_db_fixture(db):
+
+    assert db.name == pytest.NAME
+
+    db_loaded = audb.load(
+        pytest.NAME,
+        version=pytest.VERSION,
+        verbose=False,
+    )
+    assert db_loaded == db
+    assert audb.repository(pytest.NAME, pytest.VERSION) == pytest.REPOSITORY
+
+
+def test_publish_db_fixture():
+
+    db_loaded = audb.load(
+        pytest.NAME,
+        version=pytest.VERSION,
+        verbose=False,
+    )
+    assert db_loaded.name == pytest.NAME
+    assert audb.repository(pytest.NAME, pytest.VERSION) == pytest.REPOSITORY
