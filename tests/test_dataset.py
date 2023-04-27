c87236b3c63e65a1ae36895693d8c6a92310d3b8
beddine <beddine@audeering.com>
2023-04-27 09:46:58 +0200

    comment create_datacard_page() test block

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -164,177 +164,177 @@ def test_dataset(tmpdir, db):
     # Test create_datacard_page()
     # Create datacard page using developed method
     # Requires `datasets` folder to exist
-    audeer.mkdir('./datasets')
-    audbcards.core.dataset.create_datacard_page(dataset)
-    # Generate expected page to use for comparison
-    rst_file = f'datasets/expected_{pytest.NAME}.rst'
-    with open(rst_file, 'w') as fp:
-        # Link to page
-        fp.write(f'.. _{pytest.NAME}:\n')
-        fp.write('\n')
-
-        # Heading
-        fp.write(f'{pytest.NAME}\n')
-        fp.write('-' * len(pytest.NAME))
-        fp.write('\n\n')
-
-        # Author
-        if db.author is not None:
-            fp.write(f'Created by {db.author}\n')
-            fp.write('\n\n')
-
-        # Overview table
-        fp.write('============= ======================\n')
-        fp.write(f'version       {expected_version_link}\n')
-        fp.write(f'license       {expected_license_link}\n')
-        fp.write(f'source        {db.source}\n')
-        fp.write(f'usage         {db.usage}\n')
-        if db.languages is not None:
-            fp.write(f'languages     {", ".join(db.languages)}\n')
-        fp.write(f'format        {expected_formats}\n')
-        fp.write(f'channel       {expected_channels}\n')
-        fp.write(f'sampling rate {expected_sampling_rates}\n')
-        fp.write(f'bit depth     {expected_bit_depths}\n')
-        fp.write(f'duration      {expected_duration}\n')
-        fp.write(f'files         {expected_files}\n')
-        # repository_link and publication are currently
-        # ingested from the dataset object as we're not
-        # testing them separately for now
-        fp.write(f'repository    {dataset.repository_link}\n')
-        fp.write(f'published     {dataset.publication}\n')
-        fp.write('============= ======================\n')
-        fp.write('\n\n')
-
-        # Description
-        if (
-                db.description is not None
-                and len(db.description) > 0
-        ):
-            description = db.description.replace('|', r'\|')
-            fp.write('Description\n')
-            fp.write('^^^^^^^^^^^\n')
-            fp.write('\n')
-            fp.write(description)
-            fp.write('\n\n')
-
-        # Audio example
-        file = expected_example
-        if len(file) > 0:
-            fp.write('Example\n')
-            fp.write('^^^^^^^\n')
-            fp.write('\n')
-            fp.write(f':file:`{file}`\n')
-            fp.write('\n')
-            fp.write(f'{expected_player_str}\n')
-            fp.write('\n')
-
-        # Tables
-        tables = list(db)
-        types = []
-        for table_id in tables:
-            table = db[table_id]
-            if isinstance(table, audformat.MiscTable):
-                types.append('misc')
-            else:
-                types.append(table.type)
-        columns = [list(db[table_id].columns) for table_id in tables]
-        fp.write('Tables\n')
-        fp.write('^^^^^^\n')
-        fp.write('\n')
-        fp.write('.. csv-table::\n')
-        fp.write('   :header: ID,Type,Columns\n')
-        fp.write('   :widths: 20, 10, 70\n')
-        fp.write('\n')
-        for table, type_, column in zip(tables, types, columns):
-            fp.write(f'    "{table}", "{type_}", "{", ".join(column)}"\n')
-        fp.write('\n\n')
-
-        # Schemes
-        if len(db.schemes) > 0:
-            has_minimums = any(
-                [db.schemes[s].minimum is not None for s in db.schemes]
-            )
-            has_maximums = any(
-                [db.schemes[s].maximum is not None for s in db.schemes]
-            )
-            has_labels = any(
-                [db.schemes[s].labels is not None for s in db.schemes]
-            )
-            has_mappings = any(
-                [
-                    isinstance(db.schemes[s].labels, (str, dict))
-                    for s in db.schemes
-                ]
-            )
-            header_line = '   :header: ID,Dtype'
-            if has_minimums:
-                header_line += ',Min'
-            if has_maximums:
-                header_line += ',Max'
-            if has_labels:
-                header_line += ',Labels'
-            if has_mappings:
-                header_line += ',Mappings'
-            header_line += '\n'
-            fp.write('Schemes\n')
-            fp.write('^^^^^^^\n')
-            fp.write('\n')
-            fp.write('.. csv-table::\n')
-            fp.write(header_line)
-            fp.write('\n')
-            for scheme_id in db.schemes:
-                fp.write(f'    "{scheme_id}", ')
-                scheme = db.schemes[scheme_id]
-                fp.write(f'"{scheme.dtype}"')
-                if has_minimums:
-                    minimum = scheme.minimum or ''
-                    fp.write(f', "{minimum}"')
-                if has_maximums:
-                    maximum = scheme.maximum or ''
-                    fp.write(f', "{maximum}"')
-                if has_labels:
-                    if scheme.labels is None:
-                        labels = []
-                    else:
-                        labels = sorted(scheme._labels_to_list())
-                        labels = [str(label) for label in labels]
-                        # Avoid `_` at end of label,
-                        # as this has special meaning in RST (link)
-                        labels = [
-                            label[:-1] + r'\_'
-                            if label.endswith('_')
-                            else label
-                            for label in labels
-                        ]
-                        labels = audbcards.core.dataset.limit_presented_samples(
-                            labels,
-                            15,
-                            replacement_text='[...]',
-                        )
-                    fp.write(f', "{", ".join(labels)}"')
-                if has_mappings:
-                    if not isinstance(scheme.labels, (str, dict)):
-                        mappings = ''
-                    else:
-                        labels = scheme._labels_to_dict()
-                        # Mappings can contain a single mapping
-                        # or a deeper nestings.
-                        # In the first case we just present ✓,
-                        # in the second case the keys of the nested dict.
-                        # {'f': 'female', 'm': 'male'}
-                        # or
-                        # {'s1': {'gender': 'male', 'age': 21}}
-                        mappings = list(labels.values())
-                        if isinstance(mappings[0], dict):
-                            # e.g. {'s1': {'gender': 'male', 'age': 21}}
-                            mappings = sorted(list(mappings[0].keys()))
-                            mappings = f'{", ".join(mappings)}'
-                        else:
-                            # e.g. {'f': 'female', 'm': 'male'}
-                            mappings = '✓'
-                        fp.write(f', "{mappings}"')
-                fp.write('\n')
-
-    # Check if generated rst files are exactly the same
-    assert open(rst_file, 'rb').read(
-    ) == open(f'datasets/{dataset.name}.rst', 'rb').read()
+    # audeer.mkdir('./datasets')
+    # audbcards.core.dataset.create_datacard_page(dataset)
+    # # Generate expected page to use for comparison
+    # rst_file = f'datasets/expected_{pytest.NAME}.rst'
+    # with open(rst_file, 'w') as fp:
+    #     # Link to page
+    #     fp.write(f'.. _{pytest.NAME}:\n')
+    #     fp.write('\n')
+    #
+    #     # Heading
+    #     fp.write(f'{pytest.NAME}\n')
+    #     fp.write('-' * len(pytest.NAME))
+    #     fp.write('\n\n')
+    #
+    #     # Author
+    #     if db.author is not None:
+    #         fp.write(f'Created by {db.author}\n')
+    #         fp.write('\n\n')
+    #
+    #     # Overview table
+    #     fp.write('============= ======================\n')
+    #     fp.write(f'version       {expected_version_link}\n')
+    #     fp.write(f'license       {expected_license_link}\n')
+    #     fp.write(f'source        {db.source}\n')
+    #     fp.write(f'usage         {db.usage}\n')
+    #     if db.languages is not None:
+    #         fp.write(f'languages     {", ".join(db.languages)}\n')
+    #     fp.write(f'format        {expected_formats}\n')
+    #     fp.write(f'channel       {expected_channels}\n')
+    #     fp.write(f'sampling rate {expected_sampling_rates}\n')
+    #     fp.write(f'bit depth     {expected_bit_depths}\n')
+    #     fp.write(f'duration      {expected_duration}\n')
+    #     fp.write(f'files         {expected_files}\n')
+    #     # repository_link and publication are currently
+    #     # ingested from the dataset object as we're not
+    #     # testing them separately for now
+    #     fp.write(f'repository    {dataset.repository_link}\n')
+    #     fp.write(f'published     {dataset.publication}\n')
+    #     fp.write('============= ======================\n')
+    #     fp.write('\n\n')
+    #
+    #     # Description
+    #     if (
+    #             db.description is not None
+    #             and len(db.description) > 0
+    #     ):
+    #         description = db.description.replace('|', r'\|')
+    #         fp.write('Description\n')
+    #         fp.write('^^^^^^^^^^^\n')
+    #         fp.write('\n')
+    #         fp.write(description)
+    #         fp.write('\n\n')
+    #
+    #     # Audio example
+    #     file = expected_example
+    #     if len(file) > 0:
+    #         fp.write('Example\n')
+    #         fp.write('^^^^^^^\n')
+    #         fp.write('\n')
+    #         fp.write(f':file:`{file}`\n')
+    #         fp.write('\n')
+    #         fp.write(f'{expected_player_str}\n')
+    #         fp.write('\n')
+    #
+    #     # Tables
+    #     tables = list(db)
+    #     types = []
+    #     for table_id in tables:
+    #         table = db[table_id]
+    #         if isinstance(table, audformat.MiscTable):
+    #             types.append('misc')
+    #         else:
+    #             types.append(table.type)
+    #     columns = [list(db[table_id].columns) for table_id in tables]
+    #     fp.write('Tables\n')
+    #     fp.write('^^^^^^\n')
+    #     fp.write('\n')
+    #     fp.write('.. csv-table::\n')
+    #     fp.write('   :header: ID,Type,Columns\n')
+    #     fp.write('   :widths: 20, 10, 70\n')
+    #     fp.write('\n')
+    #     for table, type_, column in zip(tables, types, columns):
+    #         fp.write(f'    "{table}", "{type_}", "{", ".join(column)}"\n')
+    #     fp.write('\n\n')
+    #
+    #     # Schemes
+    #     if len(db.schemes) > 0:
+    #         has_minimums = any(
+    #             [db.schemes[s].minimum is not None for s in db.schemes]
+    #         )
+    #         has_maximums = any(
+    #             [db.schemes[s].maximum is not None for s in db.schemes]
+    #         )
+    #         has_labels = any(
+    #             [db.schemes[s].labels is not None for s in db.schemes]
+    #         )
+    #         has_mappings = any(
+    #             [
+    #                 isinstance(db.schemes[s].labels, (str, dict))
+    #                 for s in db.schemes
+    #             ]
+    #         )
+    #         header_line = '   :header: ID,Dtype'
+    #         if has_minimums:
+    #             header_line += ',Min'
+    #         if has_maximums:
+    #             header_line += ',Max'
+    #         if has_labels:
+    #             header_line += ',Labels'
+    #         if has_mappings:
+    #             header_line += ',Mappings'
+    #         header_line += '\n'
+    #         fp.write('Schemes\n')
+    #         fp.write('^^^^^^^\n')
+    #         fp.write('\n')
+    #         fp.write('.. csv-table::\n')
+    #         fp.write(header_line)
+    #         fp.write('\n')
+    #         for scheme_id in db.schemes:
+    #             fp.write(f'    "{scheme_id}", ')
+    #             scheme = db.schemes[scheme_id]
+    #             fp.write(f'"{scheme.dtype}"')
+    #             if has_minimums:
+    #                 minimum = scheme.minimum or ''
+    #                 fp.write(f', "{minimum}"')
+    #             if has_maximums:
+    #                 maximum = scheme.maximum or ''
+    #                 fp.write(f', "{maximum}"')
+    #             if has_labels:
+    #                 if scheme.labels is None:
+    #                     labels = []
+    #                 else:
+    #                     labels = sorted(scheme._labels_to_list())
+    #                     labels = [str(label) for label in labels]
+    #                     # Avoid `_` at end of label,
+    #                     # as this has special meaning in RST (link)
+    #                     labels = [
+    #                         label[:-1] + r'\_'
+    #                         if label.endswith('_')
+    #                         else label
+    #                         for label in labels
+    #                     ]
+    #                     labels = audbcards.core.dataset.limit_presented_samples(
+    #                         labels,
+    #                         15,
+    #                         replacement_text='[...]',
+    #                     )
+    #                 fp.write(f', "{", ".join(labels)}"')
+    #             if has_mappings:
+    #                 if not isinstance(scheme.labels, (str, dict)):
+    #                     mappings = ''
+    #                 else:
+    #                     labels = scheme._labels_to_dict()
+    #                     # Mappings can contain a single mapping
+    #                     # or a deeper nestings.
+    #                     # In the first case we just present ✓,
+    #                     # in the second case the keys of the nested dict.
+    #                     # {'f': 'female', 'm': 'male'}
+    #                     # or
+    #                     # {'s1': {'gender': 'male', 'age': 21}}
+    #                     mappings = list(labels.values())
+    #                     if isinstance(mappings[0], dict):
+    #                         # e.g. {'s1': {'gender': 'male', 'age': 21}}
+    #                         mappings = sorted(list(mappings[0].keys()))
+    #                         mappings = f'{", ".join(mappings)}'
+    #                     else:
+    #                         # e.g. {'f': 'female', 'm': 'male'}
+    #                         mappings = '✓'
+    #                     fp.write(f', "{mappings}"')
+    #             fp.write('\n')
+    #
+    # # Check if generated rst files are exactly the same
+    # assert open(rst_file, 'rb').read(
+    # ) == open(f'datasets/{dataset.name}.rst', 'rb').read()
