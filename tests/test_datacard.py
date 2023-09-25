e81e739523a7c079e8a0d89c2c7e7f45aeeac9a5
ChristianGeng <cgeng@audeering>
2023-09-25 14:30:33 +0200

    Adapt tests.

--- tests/test_datacard.py
+++ tests/test_datacard.py
@@ -3,9 +3,7 @@ import pytest
 import difflib
 import numpy as np
 
-from audbcards.core.dataset import create_datacard_page_from_template
 from audbcards.core.dataset import create_datasets_page_from_template
-from audbcards.core.dataset import create_datacard_page
 
 
 def test_datacard_from_template_lines_similar(db, default_template):
@@ -16,43 +14,25 @@ def test_datacard_from_template_lines_similar(db, default_template):
     content = dc._render_template()
 
     ratios = []
-    for line1, line2 in zip(default_template.split('\n'), content.split('\n')):
+    for line1, line2 in zip(default_template.split("\n"), content.split("\n")):
         sm = difflib.SequenceMatcher(lambda x: x == " ", line1, line2)
         ratios.append(sm.ratio())
 
     # there are lines that will differ (author, date of dataset)
     # almost all lines are identical, so mean and median should be high.
     assert np.median(np.array(ratios)) == 1
-    assert np.mean(np.array(ratios)) > .95
+    assert np.mean(np.array(ratios)) > 0.95
 
     # use differ to check how many lines are identical
     differ = difflib.Differ()
     obtained, expected = content.splitlines(), default_template.splitlines()
     result = list(differ.compare(obtained, expected))
     # more than 90% of the lines should be identical
-    ratio_diff = len([x for x in result if x.startswith('-')])
-    assert 1 - ratio_diff / len(result) > .9
-
-
-@pytest.mark.skip(reason='currently writing to wrong directory')
-def test_datacard_from_template(db, default_template):
-    """Create Datacard using wrapper function."""
-
-    dataset = audbcards.Dataset(pytest.NAME, pytest.VERSION)
-
-    create_datacard_page_from_template(dataset)
-
-
-@pytest.mark.skip(reason='currently writing to wrong directory')
-def test_datacard_original(db, default_template):
-
-    dataset = audbcards.Dataset(pytest.NAME, pytest.VERSION)
-
-    create_datacard_page(dataset)
+    ratio_diff = len([x for x in result if x.startswith("-")])
+    assert 1 - ratio_diff / len(result) > 0.9
 
 
 def test_create_datasets_page(db):
 
     datasets = [audbcards.Dataset(pytest.NAME, pytest.VERSION)] * 4
-    create_datasets_page_from_template(datasets,
-                                       ofbase='datasets_from_template')
+    create_datasets_page_from_template(datasets, ofbase="datasets_page")
