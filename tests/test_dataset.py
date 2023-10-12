31ece52f2a044e4ee1b8b11d1ecf5066daa48fb3
Hagen Wierstorf <hwierstorf@audeering.com>
2023-10-12 11:48:42 +0200

    Add docstring to test_dataset()

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -16,7 +16,7 @@ import audbcards
     ],
 )
 def test_dataset(audb_cache, tmpdir, repository, db, request):
-
+    r"""Test audbcards.Dataset object and all its properties."""
     db = request.getfixturevalue(db)
 
     dataset_cache = audeer.mkdir(audeer.path(tmpdir, 'cache'))
