164d482728141372759a92509d457d0b49b6898e
Hagen Wierstorf <hwierstorf@audeering.com>
2024-04-24 15:22:09 +0200

    Try to fix

--- audbcards/core/dataset.py
+++ audbcards/core/dataset.py
@@ -4,6 +4,7 @@ import os
 import pickle
 import typing
 
+# import dohq_artifactory
 import jinja2
 import pandas as pd
 
@@ -15,6 +16,19 @@ import audformat
 from audbcards.core.config import config
 from audbcards.core.utils import format_schemes
 from audbcards.core.utils import limit_presented_samples
+
+
+# def _getstate(self):
+#     return self.name
+#
+#
+# def _setstate(self, state):
+#     self.name = state
+#
+#
+# # Ensure we can pickle the repository
+# dohq_artifactory.GenericRepository.__getstate__ = _getstate
+# dohq_artifactory.GenericRepository.__setstate__ = _setstate
 
 
 class _Dataset:
@@ -81,7 +95,7 @@ class _Dataset:
         for other_version in other_versions:
             audeer.rmdir(audeer.path(self.cache_root, name, other_version))
 
-    def __get_state__(self):
+    def __getstate__(self):
         r"""Returns attributes to be pickled."""
         return self.properties
 
