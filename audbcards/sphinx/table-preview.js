f9ee635831e829ad88eae5495672066002ff7de5
Hagen Wierstorf <hwierstorf@audeering.com>
2024-07-24 13:13:37 +0200

    Mark clicked row

--- audbcards/sphinx/table-preview.js
+++ audbcards/sphinx/table-preview.js
@@ -2,7 +2,12 @@
 //
 // Implementation based on https://github.com/chhikaradi1993/Expandable-table-row
 //
-const toggleRow = (element) => {
-    element.getElementsByClassName('expanded-row-content')[0].classList.toggle('hide-row');
+const toggleRow = (row) => {
+    row.getElementsByClassName('expanded-row-content')[0].classList.toggle('hide-row');
+    if (row.className.indexOf("clicked") === -1) {
+        row.classList.add("clicked");
+    } else {
+        row.classList.remove("clicked");
+    }
     console.log(event);
 }
