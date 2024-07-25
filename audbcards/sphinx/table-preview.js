200df0ca2fb84dc2ea288feef5ac5ebb0a0a6551
Hagen Wierstorf <hwierstorf@audeering.com>
2024-07-25 08:55:44 +0200

    Extend docstring of CSS and JS file

--- audbcards/sphinx/table-preview.js
+++ audbcards/sphinx/table-preview.js
@@ -1,9 +1,13 @@
-// Expand row in Tables table to show preview of table content.
+// Expand rows in Tables table to show preview of each tables content.
 //
 // Implementation based on https://github.com/chhikaradi1993/Expandable-table-row
 //
 const toggleRow = (row) => {
+    // Toggle visibility of table preview
     row.getElementsByClassName('expanded-row-content')[0].classList.toggle('hide-row');
+    // Toggle clicked attribute on clicked table row.
+    // This can be used to adjust appearance of clicked table,
+    // e.g. remove bottom border
     if (row.className.indexOf("clicked") === -1) {
         row.classList.add("clicked");
     } else {
